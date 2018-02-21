bcf83183bda6ef5794d885658cec49a1891b2a87
Volodymyr Klekot <klekotVR@gmail.com>
2018-02-22 00:55:22 +0200

    update Movie class

--- app/src/Models/Movie.php
+++ app/src/Models/Movie.php
@@ -4,7 +4,30 @@ namespace app\src\Models;
 
 class Movie
 {
+    /**
+     * @var string
+     */
     public $title;
+    /**
+     * @var string
+     */
     public $poster;
+    /**
+     * @var string
+     */
     public $description;
+
+    /**
+     * Movie constructor.
+     * @param string $title
+     * @param string $poster
+     * @param string $description
+     */
+    public function __construct($title = 'not yet', $poster = 'not yet', $description = 'not yet')
+    {
+        $this->title = $title;
+        $this->poster = $poster;
+        $this->description = $description;
+    }
+
 }
