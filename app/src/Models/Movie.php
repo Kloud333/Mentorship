64ed27233417f8b6370d81fce3999dd5e530bff3
Volodymyr Klekot <klekotVR@gmail.com>
2018-03-05 01:37:57 +0200

    added geters and seters for Movie

--- app/src/Models/Movie.php
+++ app/src/Models/Movie.php
@@ -7,15 +7,15 @@ class Movie
     /**
      * @var string
      */
-    public $title;
+    private $title;
     /**
      * @var string
      */
-    public $poster;
+    private $poster;
     /**
      * @var string
      */
-    public $description;
+    private $description;
 
     /**
      * Movie constructor.
@@ -27,6 +27,54 @@ class Movie
     {
         $this->title = $title;
         $this->poster = $poster;
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPoster(): string
+    {
+        return $this->poster;
+    }
+
+    /**
+     * @param string $poster
+     */
+    public function setPoster(string $poster)
+    {
+        $this->poster = $poster;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description)
+    {
         $this->description = $description;
     }
 
