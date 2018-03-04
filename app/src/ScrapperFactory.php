c9f8078c394a4ed0e7f95b9335823b418e972e61
Volodymyr Klekot <klekotVR@gmail.com>
2018-03-05 01:42:48 +0200

    created ScrapperFactory

---
+++ app/src/ScrapperFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\src;
+
+use app\src\Parsers\DomCrawlerParserAdapter;
+use app\src\Parsers\FilmixParserAdapter;
+use app\src\Transporters\CurlAdapter;
+use app\src\Transporters\GuzzleAdapter;
+use Exception;
+
+class ScrapperFactory
+{
+    /**
+     * @param $domain
+     * @return Scrapper
+     * @throws Exception
+     */
+    public function create($domain)
+    {
+        switch ($domain) {
+            case 'filmix':
+                return new Scrapper(new CurlAdapter(), new FilmixParserAdapter());
+            case 'kinokrad':
+                return new Scrapper(new GuzzleAdapter(), new DomCrawlerParserAdapter());
+            default:
+                throw new Exception('Resource not found!');
+        }
+
+    }
+
+}
