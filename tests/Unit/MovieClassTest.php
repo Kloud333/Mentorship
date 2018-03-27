e9cd022c630158fc90bfe555773137065da1f993
Volodymyr Klekot <klekotVR@gmail.com>
2018-03-28 00:14:52 +0300

    added Movie class tests

--- tests/Unit/MovieClassTest.php
+++ tests/Unit/MovieClassTest.php
@@ -7,9 +7,19 @@ use app\src\Models\Movie;
 class MovieClassTest extends Base
 {
 
-    public function testMovieAttributes()
+    public function testMovieAttributeTitleExist()
     {
         $this->assertClassHasAttribute('title', Movie::class);
+    }
+
+    public function testMovieAttributePosterExist()
+    {
+        $this->assertClassHasAttribute('poster', Movie::class);
+    }
+
+    public function testMovieAttributeDescriptionExist()
+    {
+        $this->assertClassHasAttribute('description', Movie::class);
     }
 
 }
