1a36177203bc1c51276dc2522193d6ec5799d771
Volodymyr Klekot <klekotVR@gmail.com>
2018-03-29 01:34:28 +0300

    added Movie Class tests

--- tests/Unit/MovieClassTest.php
+++ tests/Unit/MovieClassTest.php
@@ -22,4 +22,31 @@ class MovieClassTest extends Base
         $this->assertClassHasAttribute('description', Movie::class);
     }
 
+    public function testMovieMethodTitle()
+    {
+        $movie_title = 'title';
+        $movie = new Movie();
+
+        $movie->setTitle($movie_title);
+        $this->assertEquals($movie_title, $movie->getTitle());
+    }
+
+    public function testMovieMethodPoster()
+    {
+        $movie_poster = 'http...';
+        $movie = new Movie();
+
+        $movie->setPoster($movie_poster);
+        $this->assertEquals($movie_poster, $movie->getPoster());
+    }
+
+    public function testMovieMethodDescription()
+    {
+        $movie_description = 'description';
+        $movie = new Movie();
+
+        $movie->setDescription($movie_description);
+        $this->assertEquals($movie_description, $movie->getDescription());
+    }
+
 }
