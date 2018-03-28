cc482460d80f32af8dbdfb2ad026a279669be6e2
Volodymyr Klekot <klekotVR@gmail.com>
2018-03-29 01:34:43 +0300

    added Parser tests

--- tests/Unit/ParsersTest.php
+++ tests/Unit/ParsersTest.php
@@ -4,19 +4,37 @@ namespace tests\Unit;
 
 use app\src\Models\Movie;
 use app\src\Parsers\FilmixParserStrategy;
+use app\src\Parsers\KinokradDomCrawlerParserAdapter;
 
 class ParsersTest extends Base
 {
 
-    public function testFilmixParser()
+    public function filmixDataProvider()
+    {
+        return [
+            ['<h1 class="name" itemprop="name">title</h1> <img src="https..." class="poster poster-tooltip" itemprop="image" /> <div class="full-story">description</div><div']
+        ];
+    }
+
+    public function kinokradDataProvider()
+    {
+        return [
+            ['<div class="fallsttitle"><h1 itemprop="name">title</h1></div><div class="bigposter"> <img title="" alt="" src="https..." itemprop=""></div><div class="fulltext" id="fulltext" itemprop="description">description</div>']
+        ];
+    }
+
+    /**
+     * @dataProvider filmixDataProvider
+     */
+    public function testFilmixParserGoodResult($data)
     {
         $scrapper = new FilmixParserStrategy();
 
-        $data = '<h1 class="name" itemprop="name">titke</h1> <img src="https://filmix.me/uploads/posters/thumbs/w220/discovery-ulichnye-gonki_104350_0.jpg" class="poster poster-tooltip" itemprop="image" /> <div class="full-story">decsription</div>';
-
         $result = $scrapper->parse($data);
 
-        $this->assertFalse(empty($result->getTitle()));
+        $this->assertEquals($result->getTitle(), 'title');
+        $this->assertEquals($result->getPoster(), 'https...');
+        $this->assertEquals($result->getDescription(), 'description');
     }
 
     public function testFilmixParserGoodResultClass()
@@ -26,5 +44,28 @@ class ParsersTest extends Base
 
         $this->assertInstanceOf(Movie::class, $result);
     }
+
+    /**
+     * @dataProvider kinokradDataProvider
+     */
+    public function testKinokradParserGoodResult($data)
+    {
+        $scrapper = new KinokradDomCrawlerParserAdapter();
+
+        $result = $scrapper->parse($data);
+
+        $this->assertEquals($result->getTitle(), 'title');
+        $this->assertEquals($result->getPoster(), 'https...');
+        $this->assertEquals($result->getDescription(), 'description');
+    }
+
+    public function testKinokradParserGoodResultClass()
+    {
+        $scrapper = new KinokradDomCrawlerParserAdapter();
+
+        $this->expectException(\InvalidArgumentException::class);
+        $result = $scrapper->parse('');
+    }
+
 
 }
